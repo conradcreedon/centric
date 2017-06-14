8d75a9165b37ea6298d44203cc3a0a32f1b65fe2
Conrad Creedon <Frost_King@Conrads-MacBook-Pro.local>
2017-06-13 22:38:05 -0700

    added members and packages

---
+++ app/models/member.js
@@ -0,0 +1,20 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+	firstName: DS.attr('string'),
+	lastName: DS.attr('string'),
+	gamerTag: DS.attr('string'),
+	age: DS.attr('number'),
+	prestige_level: DS.attr('number'),
+	location: DS.attr('string'),
+	verified: DS.attr('boolean', { defaultValue: false }),
+  createdAt: DS.attr('date', {
+    defaultValue() { return new Date(); }
+  }),
+
+  /*
+	fullName: Ember.computed('firstName', 'lastName', function() {
+    return `${this.get('firstName')} ${this.get('lastName')}`;
+  })*/
+
+});
